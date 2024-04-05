e360560587f235126224b76e6ea57ed55e2d8dd9
arpan8925 <sbxp1966@gmail.com>
2024-04-06 03:16:29 +0600

    Permenent High Score Record

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score_point = 0
-        self.highest_score = 0
+        self.read_score()
+        self.highest_score = self.def_score
         self.hideturtle()
         self.penup()
         self.speed("fastest")
@@ -17,20 +18,31 @@ class Scoreboard(Turtle):
         self.color("white")
         self.update_scoreboard()
 
+
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score_point} & High Score: {self.highest_score}", False, font=FONT)
+        self.write(f"Score: {self.score_point} & High Score: {self.def_score}", False, font=FONT)
 
     def reset(self):
-        if self.score_point > self.highest_score:
-            self.highest_score = self.score_point
+        if self.score_point > self.def_score:
+            self.def_score = self.score_point
+            self.write_score()
         self.score_point = 0
         self.update_scoreboard()
+
 
     def game_over(self):
         self.color("white")
         self.goto(-30, 0)
         self.write("GAME OVER", False, font=FONT)
+
+    def read_score(self):
+        with open("data.txt", mode="r") as file:
+            self.def_score = int(file.read())
+
+    def write_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.score_point))
 
     def increase(self):
         self.score_point += 1
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+scoreboard.get_score()
